abe59e9ea15d0e530f4ae985f2c1684dc1ba241e
trungchanh2002 <trungchanh.kda@gmail.com>
2024-01-09 11:31:19 +0700

    Create KhaoSat.js
    commit

---
+++ KhaoSat.js
@@ -0,0 +1,4 @@
+for(const x of document.querySelectorAll(".rad-dis"))
+x.checked = true;
+document.querySelector(".input-ykien").innerText = "Khong";
+document.querySelector(".btn_submit_boxes").click();
